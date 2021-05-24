b97a1417d10c4b014bb02f63bfaadb083271c4f3
Steven Kirk <grokys@gmail.com>
2021-05-24 17:00:06 +0200

    Implement equality on test structs.

--- NonBoxingValueStore/Benchmarks/SetGetValueBenchmarks.cs
+++ NonBoxingValueStore/Benchmarks/SetGetValueBenchmarks.cs
@@ -63,42 +63,102 @@ namespace Avalonia.Benchmarks
             }
         }
 
-        private struct Struct1
+        private struct Struct1 : System.IEquatable<Struct1>
         {
             public int Int1;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct1 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct1 other)
+            {
+                return Int1 == other.Int1;
+            }
         }
 
-        private struct Struct2
+        private struct Struct2 : System.IEquatable<Struct2>
         {
             public int Int1;
             public int Int2;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct2 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct2 other)
+            {
+                return Int1 == other.Int1 &&
+                       Int2 == other.Int2;
+            }
         }
 
-        private struct Struct3
+        private struct Struct3 : System.IEquatable<Struct3>
         {
             public int Int1;
             public int Int2;
             public int Int3;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct3 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct3 other)
+            {
+                return Int1 == other.Int1 &&
+                       Int2 == other.Int2 &&
+                       Int3 == other.Int3;
+            }
         }
 
-        private struct Struct4
+        private struct Struct4 : System.IEquatable<Struct4>
         {
             public int Int1;
             public int Int2;
             public int Int3;
             public int Int4;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct4 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct4 other)
+            {
+                return Int1 == other.Int1 &&
+                       Int2 == other.Int2 &&
+                       Int3 == other.Int3 &&
+                       Int4 == other.Int4;
+            }
         }
 
-        private struct Struct5
+        private struct Struct5 : System.IEquatable<Struct5>
         {
             public int Int1;
             public int Int2;
             public int Int3;
             public int Int4;
             public int Int5;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct5 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct5 other)
+            {
+                return Int1 == other.Int1 &&
+                       Int2 == other.Int2 &&
+                       Int3 == other.Int3 &&
+                       Int4 == other.Int4 &&
+                       Int5 == other.Int5;
+            }
         }
 
-        private struct Struct6
+        private struct Struct6 : System.IEquatable<Struct6>
         {
             public int Int1;
             public int Int2;
@@ -106,9 +166,24 @@ namespace Avalonia.Benchmarks
             public int Int4;
             public int Int5;
             public int Int6;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct6 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct6 other)
+            {
+                return Int1 == other.Int1 &&
+                       Int2 == other.Int2 &&
+                       Int3 == other.Int3 &&
+                       Int4 == other.Int4 &&
+                       Int5 == other.Int5 &&
+                       Int6 == other.Int6;
+            }
         }
 
-        private struct Struct7
+        private struct Struct7 : System.IEquatable<Struct7>
         {
             public int Int1;
             public int Int2;
@@ -117,9 +192,25 @@ namespace Avalonia.Benchmarks
             public int Int5;
             public int Int6;
             public int Int7;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct7 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct7 other)
+            {
+                return Int1 == other.Int1 &&
+                       Int2 == other.Int2 &&
+                       Int3 == other.Int3 &&
+                       Int4 == other.Int4 &&
+                       Int5 == other.Int5 &&
+                       Int6 == other.Int6 &&
+                       Int7 == other.Int7;
+            }
         }
 
-        private struct Struct8
+        private struct Struct8 : System.IEquatable<Struct8>
         {
             public int Int1;
             public int Int2;
@@ -128,6 +219,22 @@ namespace Avalonia.Benchmarks
             public int Int5;
             public int Int6;
             public int Int7;
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Struct8 @struct && Equals(@struct);
+            }
+
+            public bool Equals(Struct8 other)
+            {
+                return Int1 == other.Int1 &&
+                       Int2 == other.Int2 &&
+                       Int3 == other.Int3 &&
+                       Int4 == other.Int4 &&
+                       Int5 == other.Int5 &&
+                       Int6 == other.Int6 &&
+                       Int7 == other.Int7;
+            }
         }
 
         private class TestClass : AvaloniaObject
