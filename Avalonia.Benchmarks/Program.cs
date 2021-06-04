97e25a91f2ebd055b6f74388df4883e71ba2aa5d
Steven Kirk <grokys@gmail.com>
2021-06-04 17:15:02 +0200

    Added AvaloniaObject construction benchmark.

--- Avalonia.Benchmarks/Program.cs
+++ Avalonia.Benchmarks/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Avalonia.Benchmarks.Styling;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 namespace Avalonia
 {
@@ -8,12 +6,7 @@ namespace Avalonia
     {
         public static void Main(string[] args)
         {
-            var b = new Style_NonActive();
-            b.Setup();
-            Console.WriteLine("Start"); Console.ReadKey();
-            b.Toggle_NonActive_Style_Activation();
-            Console.WriteLine("End"); Console.ReadKey();
-            ///BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
